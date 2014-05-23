b5710878530fe4d88bad0d2790b0ef0b2a71b78b
callapatel <callapatel@gmail.com>
2014-05-23 09:59:27 -0700

    practice of numbers
    import math

---
+++ numbers.py
@@ -0,0 +1,53 @@
+import math
+
+print("-------------------- (1)math.ceil(x)------------------------------")
+print(math.ceil(11))
+print(math.ceil(1.2))
+
+print("-------------------- (2)math.copysign(x,y)------------------------------")
+print(math.copysign(11,-1.0))
+print(math.copysign(-5.5,1.0))
+
+print("-------------------- (3)math.fabs(x)------------------------------")
+print(math.fabs(-11))
+print(math.fabs(1.5))
+
+print("-------------------- (4)math.factorial(x)------------------------------")
+print(math.factorial(5))
+print(math.factorial(2))
+
+print("-------------------- (5)math.floor(x)------------------------------")
+print(math.floor(5.2))
+print(math.floor(5.8))
+
+print("-------------------- (6)math.fmod(x)------------------------------")
+print(math.fmod(10,2.3))
+print(math.fmod(15.6,2.4))
+
+print("-------------------- (7)math.frexp(x)------------------------------")
+print(math.frexp(10))
+print(math.frexp(2))
+
+print("-------------------- (8)math.fsum(x)------------------------------")
+#print(math.fsum(2))
+#print(math.fsum(.1))
+
+print("-------------------- (9)math.isnif(x)------------------------------")
+#print(math.isnif(10))
+#print(math.isnif(1.33))
+
+print("-------------------- (10)math.isnan(x)------------------------------")
+print(math.isnan(10))
+print(math.isnan(111390))
+
+print("-------------------- (11)math.ldexp(x)------------------------------")
+print(math.ldexp(10,1))
+print(math.ldexp(15,2))
+
+print("-------------------- (12)math.modf(x)------------------------------")
+print(math.modf(10))
+print(math.modf(15.2))
+
+print("-------------------- (13)math.trunc(x)------------------------------")
+print(math.trunc(13.54588))
+print(math.trunc(15.2764))
